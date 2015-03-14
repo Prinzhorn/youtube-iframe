b4a9a9141efa07214e00a83edbf567260bdb46d3
Prinzhorn <alex@nder-prinzhorn.de>
2015-03-14 08:35:34 +0100

    1.0.0

---
+++ README.md
@@ -0,0 +1,29 @@
+# youtube-iframe
+Wrapper for dynamically loading the YouTube iframe api script
+
+```
+npm install youtube-iframe
+```
+
+Can be used with browserify or global `window.YouTubeIframeLoader`.
+
+
+```js
+var YouTubeIframeLoader = require('youtube-iframe');
+
+YouTubeIframeLoader.load(function(YT) {
+	new YT.Player('player1', {
+		height: '390',
+		width: '640',
+		videoId: 'M7lc1UVf-VE'
+	});
+});
+
+YouTubeIframeLoader.load(function(YT) {
+	new YT.Player('player2', {
+		height: '390',
+		width: '640',
+		videoId: 'dQw4w9WgXcQ'
+	});
+});
+```
---
+++ index.js
@@ -0,0 +1,50 @@
+(function(window) {
+	var YouTubeIframeLoader = {
+		src: '//www.youtube.com/iframe_api',
+		loading: false,
+		loaded: false,
+		listeners: [],
+
+		load: function(callback) {
+			this.listeners.push(callback);
+
+			if(this.loaded) {
+				this.done();
+				return;
+			}
+
+			if(this.loading) {
+				return;
+			}
+
+			this.loading = true;
+
+			var _this = this;
+			window.onYouTubeIframeAPIReady = function() {
+				_this.loaded = true;
+				_this.done();
+			};
+
+			var script = document.createElement('script');
+			script.type = 'text/javascript';
+			script.src = this.src;
+			document.body.appendChild(script);
+		},
+
+		done: function() {
+			delete window.onYouTubeIframeAPIReady;
+
+			for(var listenerIndex = 0; listenerIndex < this.listeners.length; listenerIndex++) {
+				this.listeners[listenerIndex](window.YT);
+			}
+
+			this.listeners.length = 0;
+		}
+	};
+
+	if(typeof module !== 'undefined' && module.exports) {
+		module.exports = YouTubeIframeLoader;
+	} else {
+		window.YouTubeIframeLoader = YouTubeIframeLoader;
+	}
+}(window));
